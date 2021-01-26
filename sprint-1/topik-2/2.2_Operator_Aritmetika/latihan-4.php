caeefdaed20586feb87913d9eacae76c0fe2854e
gema <personagema4@gmail.com>
2021-01-26 08:23:36 +0700

    modfify latihan-4.php

--- sprint-1/topik-2/2.2_Operator_Aritmetika/latihan-4.php
+++ sprint-1/topik-2/2.2_Operator_Aritmetika/latihan-4.php
@@ -1,23 +1,19 @@
 <?php 
 
 echo "Barisan Aritmatika by Gema Masse Cipta\n\n";
-echo "di latihan ini kita akan belajar keren dengan cara deduktif-induktif, yoman!!!\n";
-echo "yokk lanjut\n";
 
-$a = 2;
-$b = 2;
 $n = 5;
 $n1 = 9;
 $n2 = 19;
 
-$aritmatika = $a + ($n-1) * $b;
-$aritmatika1 = $a + ($n1-1) * $b;
-$aritmatika2 = $a + ($n2-1) * $b;
+$r = ($n+1)*2;
+$r1 = ($n1+1)*2;
+$r2 = ($n2+1)*2;
 
-echo "barisan bilangan ini disebut barisan aritmatika \ndi bawah ini adalah rumus aritmatika \naritmatika = a + (n-1) * b \n\n";
-echo "1. jika n=$n, nilai r: $aritmatika \n";
-echo "2. jika n=$n1, nilai r: $aritmatika1 \n";
-echo "3. jika n=$n2, nilai r: $aritmatika2\n";
+echo "rumus yang dipakai, (n+1)*2 \n";
+echo "1. jika n=$n, nilai r: $r \n";
+echo "2. jika n=$n1, nilai r: $r1 \n";
+echo "3. jika n=$n2, nilai r: $r2\n";
 
 
 ?>
