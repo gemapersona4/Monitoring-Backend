ba1c3d9fc3dd6dcacbd3f036747b9718032198c3
gema <personagema4@gmail.com>
2021-01-27 08:15:35 +0700

    edit 3.2 latihan-1&2.php

--- sprint-1/topik-3/3.2_Nested_Perulangan_dengan_kondisi(if)/latihan-1.php
+++ sprint-1/topik-3/3.2_Nested_Perulangan_dengan_kondisi(if)/latihan-1.php
@@ -4,7 +4,7 @@ $n=9;
 
 for ($i=0; $i<$n; $i++){
     for ($j=0; $j<$n; $j++){
-        if ($j==$i) {
+        if ($j<=$i) {
             echo " + ";
         } else {
             echo " - ";
--- sprint-1/topik-3/3.2_Nested_Perulangan_dengan_kondisi(if)/latihan-2.php
+++ sprint-1/topik-3/3.2_Nested_Perulangan_dengan_kondisi(if)/latihan-2.php
@@ -1,10 +1,10 @@
 <?php
 
-$n = 9;
+$n=9;
 
 for ($i=1; $i<=$n; $i++){
     for ($j=1; $j<=$n; $j++){
-        if ($i+$j==10) {
+        if ($i+$j>=10) {
             echo " + ";
         } else {
             echo " - ";
