aebb866a9cc94ef5bb46e100cb03a646bc532b83
gema <personagema4@gmail.com>
2021-01-27 07:05:17 +0700

    kerjain tugas 3.1&3.2

---
+++ sprint-1/topik-3/3.1_Perulangan_dengan_for_dan_while/latihan-2.php
@@ -0,0 +1,9 @@
+<?php
+
+$n = 9;
+
+for ($i = 1; $i < $n; $i+=2) :
+    echo $i . " ";
+endfor;
+echo "\n";
+?>
---
+++ sprint-1/topik-3/3.2_Nested_Perulangan_dengan_kondisi(if)/latihan-1.php
@@ -0,0 +1,16 @@
+<?php
+
+$n=9;
+
+for ($i=0; $i<$n; $i++){
+    for ($j=0; $j<$n; $j++){
+        if ($j==$i) {
+            echo " + ";
+        } else {
+            echo " - ";
+        }
+    }
+    echo "\n"; 
+}
+
+?>
