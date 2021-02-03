696845d50b566451abf59e149c1ebae4d3e22ef2
gema <personagema4@gmail.com>
2021-02-03 12:58:14 +0700

    ngerjain tugas sprint-2 topik-3

--- myplayground.php
+++ myplayground.php
@@ -1,17 +1,5 @@
 <?php
 
-class hewan{
-    public static $kambing = 'Kambing memakan rumput'; //membuat vaiable static
 
-    public static function singa(){ //membuat function static
-        echo 'Singan Mengaung';
-    }
-
-}
-
-hewan::singa(); //menggunakan function static
-echo hewan::$kambing; //menggunakan varibale static
 
 ?>
-
-
---
+++ sprint-2/topik-3/latihan1.php
@@ -0,0 +1,39 @@
+<?php
+
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم 
+// ٱلْحَمْدُ لِلَّٰهِ رَبِّ ٱلْعَالَمِينَ
+// أَشْهَدُ أَنْ لَا إِلَهَ إِلَّا اللهُ وَأَشْهَدُ أَنَّ مُحَمَّدًا رَسُوْلُ اللهِ
+// اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ ، وَعَلَى آلِ مُحَمَّدٍ ، كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ ، إِنَّكَ حَمِيدٌ مَجِيدٌ
+//  ، اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ ، وَعَلَى آلِ مُحَمَّدٍ ، كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ ، وَعَلَى آلِ إِبْرَاهِيمَ ، إِنَّكَ حَمِيدٌ مَجِيدٌ
+
+class Rectangle {
+
+    public function __construct() {
+        echo "\n\n==============\n";
+        echo "|PROGRAM AREA|\n";
+        echo "==============\n";
+    }
+    
+    public static function areaRectangle($length, $width) {
+        return $length * $width;
+    }
+}
+
+function areaRec() {
+    $area = new Rectangle();
+    echo "input length = ";
+    $length = trim(fgets(STDIN));
+    echo "input width = ";
+    $width = trim(fgets(STDIN));
+    if ( $length < 0 || $width < 0 ) {
+        throw new Exception('eitsss kamu masukin angka minus, ya ngga bisa lah yaa!!!! '."\n");
+    } else {
+    echo "total area = ";
+    echo $area->areaRectangle($length, $width);
+    echo "\n";    
+    }
+}
+
+areaRec();
+
+?>
