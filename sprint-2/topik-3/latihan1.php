fdf173a2b5d7dff9a73921dc1980492c11a08ebe
gema <personagema4@gmail.com>
2021-02-04 08:44:21 +0700

    update spirint-2 topik-3 latihan1.php

--- sprint-2/topik-3/latihan1.php
+++ sprint-2/topik-3/latihan1.php
@@ -6,7 +6,7 @@
 // اللَّهُمَّ صَلِّ عَلَى مُحَمَّدٍ ، وَعَلَى آلِ مُحَمَّدٍ ، كَمَا صَلَّيْتَ عَلَى إِبْرَاهِيمَ وَعَلَى آلِ إِبْرَاهِيمَ ، إِنَّكَ حَمِيدٌ مَجِيدٌ
 //  ، اللَّهُمَّ بَارِكْ عَلَى مُحَمَّدٍ ، وَعَلَى آلِ مُحَمَّدٍ ، كَمَا بَارَكْتَ عَلَى إِبْرَاهِيمَ ، وَعَلَى آلِ إِبْرَاهِيمَ ، إِنَّكَ حَمِيدٌ مَجِيدٌ
 
-class Rectangle {
+class Area {
 
     public function __construct() {
         echo "\n\n==============\n";
@@ -17,10 +17,21 @@ class Rectangle {
     public static function areaRectangle($length, $width) {
         return $length * $width;
     }
+
+    public static function areaCircle($radius) {
+        return pi() * $radius**2;
+    }
+
+    public static function areaTrapezoid($up, $down, $height) {
+        return ($up + $down) * $height / 2;
+    }
 }
 
 function areaRec() {
-    $area = new Rectangle();
+    $area = new Area();
+    echo "\n\n========================\n";
+        echo "|PROGRAM RECTANGLE AREA|\n";
+        echo "========================\n";
     echo "input length = ";
     $length = trim(fgets(STDIN));
     echo "input width = ";
@@ -32,8 +43,45 @@ function areaRec() {
     echo $area->areaRectangle($length, $width);
     echo "\n";    
     }
+} 
+function areaCir() {
+    $area = new Area();
+    echo "\n\n=====================\n";
+    echo "|PROGRAM CIRCLE AREA|\n";
+    echo "=====================\n";
+    echo "input radius = ";
+    $radius = trim(fgets(STDIN));
+    if ( $radius < 0 ) {
+        throw new Exception('eitsss kamu masukin angka minus, ya ngga bisa lah yaa!!!! '."\n");
+    } else {
+    echo "total area = ";
+    echo $area->areaCircle($radius);
+    echo "\n";    
+    }
+}
+
+function areaTra() {
+    $area = new Area();
+    echo "\n\n========================\n";
+    echo "|PROGRAM TRAPEZOID AREA|\n";
+    echo "========================\n";
+    echo "input up = ";
+    $up = trim(fgets(STDIN));
+    echo "input down = ";
+    $down = trim(fgets(STDIN));
+    echo "input height = ";
+    $height = trim(fgets(STDIN));
+    if ( $up < 0 || $down < 0 || $height < 0 ) {
+        throw new Exception('eitsss kamu masukin angka minus, ya ngga bisa lah yaa!!!! '."\n");
+    } else {
+    echo "total area = ";
+    echo $area->areaTrapezoid($up, $down, $height);
+    echo "\n";    
+    }
 }
 
 areaRec();
+areaCir();
+areaTra();
 
 ?>
